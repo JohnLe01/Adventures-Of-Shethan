d5b3f12f20d1dfe2e74e552b410eb3ab252f1cc1
John Le <johnminhle01@gmail.com>
2017-05-19 14:51:27 -0400

    Redo project layout

---
+++ src/Assets/Script/SceneController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneController : MonoBehaviour {
+    public int enemyCount = 5;
+	[SerializeField] private GameObject[] enemyPrefab;
+    public GameObject[] _enemy;
+
+    void Update() {
+        for (int i = 0; i < enemyCount; i++) {
+            if (_enemy[i] == null) {
+                _enemy[i] = Instantiate (enemyPrefab[i]) as GameObject;
+                float xValue = Random.Range (95, 250);
+                float zValue = Random.Range(110, 250);
+                _enemy[i].transform.position = new Vector3 (xValue, 60, zValue);
+                float angle = Random.Range (0, 360);
+                _enemy[i].transform.Rotate (0, angle, 0);
+            }
+        }
+    }
+}
---
+++ src/Assets/Script/WanderingAI.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WanderingAI : MonoBehaviour {
+	public const float baseSpeed = 3.0f;
+
+	public float speed = 3.0f;
+	public float obstacleRange = 5.0f;
+		
+	private bool _alive;
+
+    private Animator animator;
+
+	void Awake() {
+		Messenger<float>.AddListener(GameEvent.SPEED_CHANGED, OnSpeedChanged);
+	}
+	void OnDestroy() {
+		Messenger<float>.RemoveListener(GameEvent.SPEED_CHANGED, OnSpeedChanged);
+	}
+
+	void Start() {
+		_alive = true;
+        animator = GetComponent<Animator>();
+	}
+	
+	void Update() {
+		if (_alive) {
+            animator.SetBool("Hop", true);
+			transform.Translate(0, 0, speed * Time.deltaTime);
+			
+			Ray ray = new Ray(transform.position, transform.forward);
+			RaycastHit hit;
+			if (Physics.SphereCast(ray, 0.75f, out hit)) {
+                if (hit.distance < obstacleRange || this.transform.position.x <= 80.0f) {
+					float angle = Random.Range(-180, 180);
+					transform.Rotate(0, angle, 0);
+				}
+			}
+		}
+	}
+
+	public void SetAlive(bool alive) {
+		_alive = alive;
+	}
+
+    public bool IsAlive() {
+        return _alive;
+    }
+
+	private void OnSpeedChanged(float value) {
+		speed = baseSpeed * value;
+	}
+}
